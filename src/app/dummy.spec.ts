31607903f7eac037d36676e9af9dfce5cb960eaf
John Olanipekun <johnolanipekun@Johns-MacBook-Pro.local>
2026-05-06 18:52:03 +0100

    test: add explicit spec file to satisfy tsconfig

---
+++ src/app/dummy.spec.ts
@@ -0,0 +1,5 @@
+describe('Initial CI Check', () => {
+  it('should pass this dummy test to verify the pipeline', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ src/test.spec.ts
@@ -0,0 +1,5 @@
+describe('CI-Check', () => {
+  it('should verify the testing environment is active', () => {
+    expect(true).toBe(true);
+  });
+});
